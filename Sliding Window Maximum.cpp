544cece478953ca0ecdf803f9187721364e2b9c8
Deepak Bhardwaj <99780500+Deepak17460@users.noreply.github.com>
2022-10-25 11:53:00 +0530

    Update Sliding Window Maximum.cpp

--- Sliding Window Maximum.cpp
+++ Sliding Window Maximum.cpp
@@ -25,3 +25,36 @@ vector<int> Solution::slidingMaximum(const vector<int> &A, int B) {
     }
     return res;
 }
+// Gfg Approach
+class Solution
+{
+  public:
+    //Function to find maximum of each subarray of size k.
+    vector <int> max_of_subarrays(int *arr, int n, int k)
+    {
+    vector<int>res;
+    list<int>l;
+    //int B=k;
+    int i=0,j=0;
+    //int n=A.size();
+    while(j<n){
+        if(l.empty()){
+            l.push_back(arr[j]);
+        }
+        else{
+            while(!l.empty() && l.back()<arr[j]){
+                l.pop_back();
+            }
+            l.push_back(arr[j]);
+        }
+        if(j-i+1<k) j++;
+        else if(j-i+1==k){
+            res.push_back(l.front());
+            if(l.front()==arr[i]) l.pop_front();
+            i++;
+            j++;
+        }
+    }
+    return res;
+    }
+};
